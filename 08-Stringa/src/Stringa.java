618c51c19103cc34c11cc10411ebd82d86a9694a
AleLoffredo <148055925+AleLoffredo@users.noreply.github.com>
2023-10-16 17:55:21 +0200

    Add files via upload

---
+++ 08-Stringa/src/Stringa.java
@@ -0,0 +1,26 @@
+//(stringa primitiva e a ogg)String S è diversa da quella di tipo primitivo facendo int a creiamo una zona di memoria di nome a 
+//a=10 metterà nella zona a 10
+//array: int[]v;
+//int v[];
+//int v [null]; v=new int[4] metterà l'indirizzo di memoria della prima v[2]=10;
+//metodi
+//v=new int [4],v null
+public class Stringa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	/*String S1=new string("ciao");
+	String S2= new string("ciao");
+	System.out.print(S1==S2);
+	// sbagliato
+	 */
+   
+		String S1="ciao";
+		String S2="ciao";
+		System.out.print(S1==S2);
+	
+		//S1.equals(S2) per richiamare S2
+}
+}
+//lettura di un array di strnghe e stamparlo
