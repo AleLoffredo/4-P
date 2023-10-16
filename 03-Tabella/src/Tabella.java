f66e7ef343f8b076e3e5b63bcc419931938d0692
AleLoffredo <148055925+AleLoffredo@users.noreply.github.com>
2023-10-16 12:00:46 +0200

    Add files via upload

---
+++ 03-Tabella/src/Tabella.java
@@ -0,0 +1,15 @@
+
+public class Tabella {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		System.out.print("Persona altezza (cm)    Misura Scarpe \n");
+		System.out.print("=============================================\n");
+		System.out.print("Luisa  160 \t\t 36 \nMarco  180 \t\t 43 \nGiulia 168 \t\t 38");
+		*/
+		System.out.println("Persona altezza (cm)   Misura Scarpe\n ===================================\nLuisa 160 \t\t 36 \nMarco 180 \t\t 43 \nGiulia 168 \t\t 38");
+
+	}
+
+}
